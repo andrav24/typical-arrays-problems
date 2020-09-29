6fd3055ecce461bf04dda1f6dea092f050ad3470
andrav24 <andrav24@yandex.ru>
2020-09-29 22:17:16 +0700

    refactor: delete initial value in array.reduce. This value is first array element by default

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
     if (arguments.length === 0 || array.length === 0) return 0;
-    return array.reduce((acc,val) => val < acc ? val : acc, array[0]);
+    return array.reduce((acc,val) => val < acc ? val : acc);
 }
 
 exports.max = function max (array) {
     if (arguments.length === 0 || array.length === 0) return 0
-    return array.reduce((acc,val) => val > acc ? val : acc, array[0])
+    return array.reduce((acc,val) => val > acc ? val : acc)
 }
 
 exports.avg = function avg (array) {
